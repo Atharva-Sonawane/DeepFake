86ded34ef6992237f305df2cc7a23c0b36713c25
Atharva-Sonawane <atharvasonawane8080@gmail.com>
2025-09-30 00:05:52 +0530

    Delete Deployment_ready.py.py

--- Deployment_ready.py.py
+++
@@ -1,6 +0,0 @@
-def predict(input):
-    # Auto-detect input type
-    if input.ndim == 4:  # Image
-        return model(image_batch)
-    elif input.ndim == 5:  # Video
-        return model(video_frames).mean(dim=1)  # Temporal average
